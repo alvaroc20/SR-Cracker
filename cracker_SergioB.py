244e58b8a7eedaad62479102aeeb99568e2742f8
alvaroc20 <vivamatlab@gmail.com>
2020-11-25 11:38:26 +0100

    prueba

--- cracker_SergioB.py
+++ cracker_SergioB.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 # -*- coding:utf-8; mode:python -*-
-
 import string
 import math
-import time
-import multiprocessing
-import subprocess
-from os import remove
-from os import path
 from time import time
+from multiprocessing import Process, cpu_count
+import gnupg
+import subprocess
 
-
+subprocess.run(args=['pip3', "install", "progress"])
+subprocess.run(args=['pip3', "install", "gnupg"])
+from progress.bar import IncrementalBar
 
 def to_base(n, alphabet, width):
     """
@@ -24,7 +23,6 @@ def to_base(n, alphabet, width):
     retval = ''
     base = len(alphabet)
 
-
     while n >= base:
         index = n % base
         n = n // base
@@ -35,56 +33,59 @@ def to_base(n, alphabet, width):
     return "{:{}>{}}".format(retval[::-1], alphabet[0], width)
 
 
-def gen_key_partitions(alphabet, key_len, n_partitions):
-    key_space = len(alphabet) ** key_len
-
-    partition_size = math.ceil(key_space / n_partitions)
-    print("Partition size = key space / #partitions = {:,} / {} = {:,}".format(
-        key_space, n_partitions, partition_size))
+def gen_key_partitions(key_space, partition_size):
 
     for first in range(0, key_space, partition_size):
         last = min(first + partition_size, key_space) -1
         yield first, last
 
-def keyDiccionary(first, last, alphabet):
-    #Genera la secuencia de de combinaciones
-    for key in range(first, last+1):
-        password= "{}".format(to_base(key, alphabet, key_len))
-        #print("FIRTS {} , LAST {}: PASSWORD {}\n".format(first,last, password))
-        pass_a = subprocess.run(args=['gpg', '--ignore-mdc-error', '-d', '--batch', '--output', '/home/warrios/Escritorio/universidad/seguridad_Redes/cracker/trabajos-teoría.pdf','--passphrase', password,
-                                      '/home/warrios/Escritorio/universidad/seguridad_Redes/cracker/trabajos-teoría.pdf.gpg-no-mdc'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def gpg_Process(first, last, bar):
+    #Genera la secuencia de de combinaciones para la CPU x
+    for key in range(first,last+1):
+        password ='{}'.format(to_base(key, alphabet, key_len)) #print(password)
+        status = gpg.decrypt_file(open('trabajos-teoría.pdf.gpg-no-mdc', 'rb'), passphrase=password, output='trabajos-teoría.pdf')
 
-        not_Password = '{!r}'.format('gpg: datos cifrados CAST5\ngpg: cifrado con 1 frase contraseña\ngpg: descifrado fallido: Clave incorrecta de sesión\n')
-        intento_pass = '{!r}'.format(pass_a.stderr.decode('utf-8'))
-        if len(not_Password) != len(intento_pass):
-            print('Eureka! la password es: ',password)
-
-        #print(combinacion)
-
-
-alphabet = string.ascii_lowercase # Si quieres que la encuentre rapido'buz'
-print(alphabet)
+        if status.ok:
+            print("\033[4;32m\n########################################\n")
+            print("La password es: ",password)
+            print("\n########################################\n\033")
+        bar.next()
+        
+#Tipo de valores a combinar
+alphabet = 'buz'#string.ascii_lowercase # Si quieres que la encuentre rapido'buz'
+#Tamaño de la cadena a formar
 key_len = 4
+key_space = len(alphabet) ** key_len
+n_partition = cpu_count()
+partition_size = math.ceil(key_space / n_partition)
+
+print("Partition size = key space / partitions = {:,} / {} = {:,}".format(key_space, n_partition, partition_size))
+
+bar = IncrementalBar('', max = partition_size)
+gpg = gnupg.GPG(options= '--ignore-mdc-error',)
+jobs =[]
 
 
-if path.exists("diccionario.txt"):
-    remove('diccionario.txt')
+#Inicialización crackeo
 start_time = time()
-
-jobs = []
-for first, last, in gen_key_partitions(alphabet, key_len=key_len, n_partitions=multiprocessing.cpu_count()):
+for first, last in gen_key_partitions(key_space=key_space, partition_size=partition_size):  
+    #Rango i de valores a combinar
     print("- {:>20,} {:>20,} -{:>20,}".format(
         last-first, first, last), end='')
     print(" {:>10} -{:>10}".format(to_base(first, alphabet, key_len), to_base(last, alphabet, key_len)))
+
+    #gpg_Process(first, last, bar)
     
-    p = multiprocessing.Process(target=keyDiccionary, args=(first,last, alphabet))
+    p = Process(target=gpg_Process, args=(first, last, bar))
     jobs.append(p)
-    p.start()
 
-
-
+#Inicialización procesos
 for j in jobs:
-    j.join()
+    j.start()
+
+#Espera finalización procesos
+for j in jobs:
+    p.join()
 
 elapsed_time = time() - start_time
 print("Elapsed time: %0.10f seconds." % elapsed_time)
